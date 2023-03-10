b9eb2d95e21d9918cc0708cd5792d3963852b475
stefvannieuwenhove <stef.vannieuwenhove@telenet.be>
2023-03-10 11:16:28 +0100

    pdf

---
+++ H03/H03-Deling/H03-Deling/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+Console.Write("Geef getal 1: ");
+int getal1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Geef getal 2: ");
+int getal2 = Convert.ToInt32(Console.ReadLine());
+
+try {
+    int resultaat = getal1 / getal2;
+    Console.WriteLine("Resultaat: " + resultaat);
+}
+catch (DivideByZeroException ex) {
+    Console.WriteLine("Je kan niet delen door nul!");
+}
+catch (Exception ex) {
+    Console.WriteLine("Er is een fout opgetreden!");
+}
