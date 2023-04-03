df2dc0848e9709194079d3467c161bdbf16f93fb
stefvannieuwenhove <stef.vannieuwenhove@telenet.be>
2023-04-03 16:30:11 +0200

    H05

--- H05/H05-StringCalculator/Calculator.Domain/Model/Calculator.cs
+++ H05/H05-StringCalculator/Calculator.Domain/Model/Calculator.cs
@@ -1,5 +1,22 @@
+using System.Text.RegularExpressions;
+
 namespace Calculator.Domain.Model; 
 
 public class Calculator {
-    
+
+    private const string DELIMITER = ",";
+
+    public int Add(string value) {
+
+        string[] values = value.Split(DELIMITER);
+        Regex regex = new Regex(@"[0-9]");
+        int result = 0;
+
+        foreach (string item in values) {
+            if (regex.IsMatch(item))
+                result+= int.Parse(item);
+        }
+
+        return result;
+    }
 }
--- H05/H05-StringCalculator/Calculator.Test/CalculatorTest.cs
+++ H05/H05-StringCalculator/Calculator.Test/CalculatorTest.cs
@@ -1,9 +1,16 @@
 namespace Calculator.Test; 
+using Calculator.Domain.Model;
 
 public class CalculatorTest {
 
     [Theory]
-    public void Calculator_ValidValues_Return() {
+    [InlineData("", 0)]
+    [InlineData("7", 7)]
+    [InlineData("5,7", 12)]
+    public void Calculator_ParseableString_ValidInteger(string input, int expected) {
+        Calculator calculator = new Calculator();
 
+        Assert.Equal(expected, calculator.Add(input));
     }
+
 }
